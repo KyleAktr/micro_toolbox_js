c78910699934fa54691cb390d3f987245a64cfc2
KyleAktr <aktr.contact@gmail.com>
2025-03-31 11:14:42 +0200

    calculatrice done

--- js/calculatrice.js
+++ js/calculatrice.js
@@ -8,17 +8,60 @@ const soustraction = (a, b) => a - b;
 const multiplication = (a, b) => a * b;
 const division = (a, b) => a / b;
 
+function calculerDirect() {
+  if (!a || !b || !operateur) {
+    return;
+  }
+
+  let nbrA = parseFloat(a);
+  let nbrB = parseFloat(b);
+
+  switch (operateur) {
+    case "+":
+      resultat = addition(nbrA, nbrB);
+      break;
+    case "-":
+      resultat = soustraction(nbrA, nbrB);
+      break;
+    case "*":
+      resultat = multiplication(nbrA, nbrB);
+      break;
+    case "/":
+      if (tempB === 0) {
+        resultat = "Erreur";
+        break;
+      }
+      resultat = division(nbrA, nbrB);
+      break;
+  }
+
+  if (typeof resultat === "number") {
+    resultat = Number(resultat.toFixed(4));
+  }
+
+  document.getElementById("resultat").value = resultat;
+}
+
 function ajouter(valeur) {
   if (valeur === "+" || valeur === "-" || valeur === "*" || valeur === "/") {
+    if (a && b && operateur) {
+      calculerDirect();
+      a = resultat.toString();
+      b = "";
+    }
     operateur = valeur;
-    document.getElementById("resultat").value += " " + valeur + " ";
+    document.getElementById("resultat-direct").value = "";
+    document.getElementById("resultat-direct").value += " " + valeur + " ";
   } else {
     if (!operateur) {
       a += valeur;
     } else {
       b += valeur;
     }
-    document.getElementById("resultat").value += valeur;
+    document.getElementById("resultat-direct").value += valeur;
+    if (a && b && operateur) {
+      calculerDirect();
+    }
   }
 }
 
@@ -52,14 +95,27 @@ function calculer() {
       resultat = "Erreur";
   }
 
+  if (typeof resultat === "number") {
+    resultat = Number(resultat.toFixed(4));
+  }
+
   document.getElementById("resultat").value = resultat;
   a = resultat.toString();
   b = "";
   operateur = "";
 }
 
+function effacerDirect() {
+  document.getElementById("resultat-direct").value = "";
+  // a = "";
+  // b = "";
+  // operateur = "";
+  // resultat = "";
+}
+
 function effacer() {
   document.getElementById("resultat").value = "";
+  document.getElementById("resultat-direct").value = "";
   a = "";
   b = "";
   operateur = "";
